76c9b2be2c5ddd46fe1893d37e0ccf52c2b5db05
Galamaleiko <59849308+Galamaleiko@users.noreply.github.com>
2020-05-20 14:38:31 +0300

    Add files via upload

---
+++ Module_ASD_Babiichuk/src/uni/Task1.java
@@ -0,0 +1,105 @@
+package uni;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Stack;
+
+//Задача 1. Створити стек цілочисельних значень. На початку програма
+//зчитує три значення (N1, N2, N3 кількість елементів в стеку). В наступних трьох
+//рядках вводимо значення ваги кожного елемента стеку.
+//Програма повинна визначити чи можна отримати рівне значення в усіх
+//трьох стеках видаляючі зайві елементи.
+//Результатом роботи має бути виведення максимальної спільної ваги при
+//якій стеки будуть дорівнювати одне одному та самі стеки до та після
+//вирівнювання.
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter N1, N2 and N3 :");
+        int sizeFirst = sc.nextInt(), sizeSecond = sc.nextInt(), sizeThird = sc.nextInt(), valueFirst = 0, valueSecond = 0, valueThird = 0;
+
+        Stack<Integer> stackFirst = new Stack<Integer>();
+        ArrayList<Integer> listFirst = new ArrayList<>();
+        Stack<Integer> stackSecond = new Stack<Integer>();
+        ArrayList<Integer> listSecond = new ArrayList<>();
+        Stack<Integer> stackThird = new Stack<Integer>();
+        ArrayList<Integer> listThird = new ArrayList<>();
+
+        for (int i = 0; i < sizeFirst; i++) {
+            System.out.println("Enter element to add to first stack :");
+            int temp = sc.nextInt();
+            listFirst.add(temp);
+            stackFirst.push(temp);
+            valueFirst += temp;
+        }
+
+        for (int i = 0; i < sizeSecond; i++) {
+            System.out.println("Enter element to add to second stack :");
+            int temp = sc.nextInt();
+            listSecond.add(temp);
+            stackSecond.push(temp);
+            valueSecond += temp;
+        }
+
+        for (int i = 0; i < sizeThird; i++) {
+            System.out.println("Enter element to add to third stack :");
+            int temp = sc.nextInt();
+            listThird.add(temp);
+            stackThird.push(temp);
+            valueThird += temp;
+        }
+
+        System.out.println("Stacks before equalisation:");
+        System.out.println(listFirst + " : value " + valueFirst);
+        System.out.println(listSecond + " : value " + valueSecond);
+        System.out.println(listThird + " : value " + valueThird);
+
+        int valueMin = Integer.MAX_VALUE;
+        while (true) {
+            if (valueFirst < valueMin) {
+                valueMin = valueFirst;
+            } else if (valueSecond < valueMin) {
+                valueMin = valueSecond;
+            } else if (valueThird < valueMin) {
+                valueMin = valueThird;
+            }
+
+            while (valueFirst > valueMin) {
+                int temp = stackFirst.pop();
+                valueFirst = valueFirst - temp;
+                if (valueFirst < valueMin) {
+                    valueMin = valueFirst;
+                }
+
+            }
+            while (valueSecond > valueMin) {
+                int temp = stackSecond.pop();
+                valueSecond = valueSecond - temp;
+                if (valueSecond < valueMin) {
+                    valueMin = valueSecond;
+                }
+
+            }
+            while (valueThird > valueMin) {
+                int temp = stackThird.pop();
+                valueThird = valueThird - temp;
+                if (valueThird < valueMin) {
+                    valueMin = valueThird;
+                }
+
+            }
+            if (valueMin > 0 && valueFirst == valueSecond && valueThird == valueSecond) {
+                System.out.println("Result value is " + valueMin + ", stacks :");
+                System.out.println(stackFirst);
+                System.out.println(stackSecond);
+                System.out.println(stackThird);
+                break;
+            } else if (valueMin <= 0){
+                System.out.println("Condition can't be accessed.");
+                break;
+            }
+        }
+    }
+}
---
+++ Module_ASD_Babiichuk/src/uni/Task2.java
@@ -0,0 +1,104 @@
+package uni;
+
+import java.util.Scanner;
+
+//Задача 2. Створити двозв’язний список цілочисельних значень. На
+//початку ввести кількість значень N, що будуть прийняті в список та кількість
+//значень Add, що будуть до нього додані.
+//Ввести необхідні значення у список, після останнього список має
+//виводитись автоматично. Після цього додати значення до списку, відсортувати
+//та вивести його
+
+class ListElement {
+    int value;
+    ListElement next;
+    ListElement previous;
+
+    ListElement() {
+    }
+
+    ListElement(int value) {
+        this.value = value;
+    }
+}
+
+class newList {
+
+    ListElement head = null, tail = null;
+
+    public void create(int startAmount) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < startAmount; i++) {
+            System.out.println("Enter " + (i + 1) + " element");
+            ListElement temp = new ListElement(sc.nextInt());
+
+            if (tail == null) {
+                tail = temp;
+                head = temp;
+            } else {
+                tail.next = temp;
+                temp.previous = tail;
+                tail = temp;
+            }
+        }
+    }
+
+    public void add(int startAmount) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < startAmount; i++) {
+            System.out.println("Enter element to add :");
+            ListElement temp = new ListElement(sc.nextInt());
+            tail.next = temp;
+            temp.previous = tail;
+            tail = temp;
+        }
+
+    }
+
+    public void show() {
+        ListElement temp = head;
+        while (temp != null) {
+            System.out.print(temp.value + " ");
+            temp = temp.next;
+
+        }
+        System.out.println();
+    }
+
+
+    public void sort(int startAmount, int addAmount) {
+
+        int size = startAmount + addAmount;
+
+        for (int i = 0; i < size - 1; i++) {
+            ListElement curr = head;
+            while (curr.next != null) {
+                if (curr.value > curr.next.value) {
+                    int tempValue = curr.value;
+                    curr.value = curr.next.value;
+                    curr.next.value = tempValue;
+                }
+                curr = curr.next;
+            }
+        }
+    }
+
+}
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        newList List = new newList();
+        System.out.println("Enter N, Add :");
+        int startAmount = sc.nextInt();
+        int addAmount = sc.nextInt();
+        List.create(startAmount);
+        List.show();
+        List.add(addAmount);
+        List.sort(startAmount, addAmount);
+        System.out.println("Sorted List :");
+        List.show();
+
+    }
+}
